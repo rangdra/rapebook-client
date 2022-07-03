b18629ae51f79765cd7e3ba5705337b26f9872ef
rangdra <rangdra2@gmail.com>
2022-07-03 09:47:02 +0700

    Create .env

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL =
+  process.env.REACT_APP_NODE_ENV === 'production'
+    ? 'https://rapebook-server.herokuapp.com/api'
+    : 'http://localhost:5000/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
